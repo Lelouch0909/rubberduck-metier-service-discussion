97b6a2f29e654e7e603c913a6f04e55ca8469e69
Lelouch0909 <lnhm87404@gmail.com>
2025-03-15 17:04:21 +0100

    Initial commit

---
+++ src/main/java/com/lontsi/rubberduckmetierservicediscussion/RubberduckMetierServiceDiscussionApplication.java
@@ -0,0 +1,13 @@
+package com.lontsi.rubberduckmetierservicediscussion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubberduckMetierServiceDiscussionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RubberduckMetierServiceDiscussionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lontsi/rubberduckmetierservicediscussion/RubberduckMetierServiceDiscussionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lontsi.rubberduckmetierservicediscussion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubberduckMetierServiceDiscussionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
